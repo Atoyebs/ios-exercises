d65995fb5e2ad2ab700e94bd322baac1483aa7d3
Inioluwa Atoyebi <ini@waffleapp.co>
2016-04-15 17:53:25 +0100

    Created first UI app (Calcohol)
    Calculates alcohol content (from Bloc.io) also finished the assignment
    to update slider as it is dragged

---
+++ Calcohol/Calcohol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calcohol
+//
+//  Created by Inioluwa Work Account on 15/04/2016.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calcohol/Calcohol/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calcohol
+//
+//  Created by Inioluwa Work Account on 15/04/2016.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
