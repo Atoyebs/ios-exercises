983d7f8a9d6d831bd477ef15f80af010b101d320
Inioluwa Atoyebi <ini@waffleapp.co>
2016-04-20 12:53:40 +0100

    finished step 23 in bloc.io training
    Finished bloc.io checkpoint 23 and assignment in making alert popup

--- BlocBrowser/BlocBrowser/AppDelegate.m
+++ BlocBrowser/BlocBrowser/AppDelegate.m
@@ -37,6 +37,10 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
+    UINavigationController *navigationVC = (UINavigationController *)self.window.rootViewController;
+    BLCWebBrowserViewController *browserVC = [[navigationVC viewControllers] firstObject];
+    [browserVC resetView];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -50,6 +54,25 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+//    NSLog(@"Application has come back in");
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Welcome" message:@"Get Excited To Use The Best Web Browser Ever" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Excited" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        
+        [alert dismissViewControllerAnimated:YES completion:nil];
+        
+    }];
+    
+    [alert addAction:okAction];
+    
+    UINavigationController *navigationVC = (UINavigationController *)self.window.rootViewController;
+    
+    [navigationVC presentViewController:alert animated:YES completion:nil];
+    
+    
+    
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
