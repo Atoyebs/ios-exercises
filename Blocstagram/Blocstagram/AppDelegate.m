62390ddb31c0d4196be01d95b0eebbe733b2fbac
Inioluwa Atoyebi <ini@waffleapp.co>
2016-04-21 17:44:01 +0100

    Removed Redundant Files & Data
    Beginning of refactoring

--- Blocstagram/Blocstagram/AppDelegate.m
+++ Blocstagram/Blocstagram/AppDelegate.m
@@ -18,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
     // Override point for customization after application launch.
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[BLCImagesTableViewController new]];
     
