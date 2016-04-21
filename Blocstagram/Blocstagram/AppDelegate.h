eb7733b47ddcbe43ab62db18552d2c5e623d92e7
Inioluwa Atoyebi <ini@waffleapp.co>
2016-04-21 16:11:17 +0100

    Added Blocstagram project (default files & settings)

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Inioluwa Work Account on 21/04/2016.
+//  Copyright © 2016 bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
