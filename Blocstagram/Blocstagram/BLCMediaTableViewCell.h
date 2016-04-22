925a1850130aa8d33689c8aa236357c6e5005950
Inioluwa Atoyebi <ini@waffleapp.co>
2016-04-22 14:38:23 +0100

    Added UITableViewCell subclass
    Added a new class BLCMediaTableViewCell which is a subclass of
    UITableViewCell

---
+++ Blocstagram/Blocstagram/BLCMediaTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BLCMediaTableViewCell.h
+//  Blocstagram
+//
+//  Created by Inioluwa Work Account on 22/04/2016.
+//  Copyright © 2016 bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCMediaTableViewCell : UITableViewCell
+
+@end
---
+++ Blocstagram/Blocstagram/BLCMediaTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  BLCMediaTableViewCell.m
+//  Blocstagram
+//
+//  Created by Inioluwa Work Account on 22/04/2016.
+//  Copyright © 2016 bloc. All rights reserved.
+//
+
+#import "BLCMediaTableViewCell.h"
+
+@implementation BLCMediaTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
